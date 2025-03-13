0171c58971870d29a0d72ad71266f6452cd9800c
tauhidulcog <tauhidul@cogwheelanalytics.com>
2025-03-13 22:25:51 +0600

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TourNgxBootstrapModule, TourService} from 'ngx-ui-tour-ngx-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TourNgxBootstrapModule],
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'guide-tour-with-popper';
+  constructor(public tourService: TourService) {
+    this.tourService.initialize([
+      {
+        anchorId: 'angular-ui-tour',
+        content:
+          'Thanks to angular-ui-tour for the inspiration for the library',
+        placement: 'bottom',
+        title: 'angular-ui-tour'
+      },
+      {
+        anchorId: 'home-page',
+        content: 'This is home page',
+        placement: 'bottom',
+        title: 'angular-ui-tour'
+      }
+    ])
+    this.tourService.start();
+  }
+
+
 }
